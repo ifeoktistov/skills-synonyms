08b06bb4940afb83f34351566cbe4841ec43f097
Igor Feoktistov <feoktistov.igor@gmail.com>
2013-11-26 14:20:07 +0200

    create input csv file

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 include "work.php";
+
+$server_result = '';
+if (isset($_POST['type'])) {
+    $server_result = Work::processPOST();
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,17 +20,17 @@ include "work.php";
 <div class="container">
     <h3>Hello, skills-synonyms!</h3>
 
-    <?php if (isset($_POST['type'])):?>
+    <?php if ($server_result):?>
     <div class="alert alert-info">
         <h4>Server result: </h4>
-        <?php echo Work::processPOST(); ?>
+        <?php echo $server_result; ?>
     </div>
     <?php endif;?>
 
     <div class="navbar">
         <div class="navbar-inner">
             <form class="navbar-form form-inline" method="post">
-                <label class="checkbox inline">Get synonyms:</label>
+                <label class="checkbox inline">Type word:</label>
                 <input type="text" class="span2" name="word">
                 <input type="hidden" name="type" value="api-test">
                 <button type="submit" class="btn">Test API</button>
@@ -38,13 +44,21 @@ include "work.php";
                 <label class="checkbox inline">List:</label><br/>
                 <textarea rows="3" name="list"></textarea>
                 <input type="hidden" name="type" value="list"><br/>
-                <button type="submit" class="btn">Get synonyms</button>
+                <button type="submit" class="btn">Get Synonyms</button>
             </form>
         </div>
     </div>
 
-
-
+    <div class="navbar">
+        <div class="navbar-inner">
+            <form class="navbar-form form-inline" method="post" enctype="multipart/form-data">
+                <label class="checkbox inline">Input CSV file:</label>
+                <input type="file" class="span6" name="file">
+                <input type="hidden" name="type" value="csv-file"><br/>
+                <button type="submit" class="btn">Parse File</button>
+            </form>
+        </div>
+    </div>
 </div>
 
 <script src="http://code.jquery.com/jquery.js"></script>
--- work.php
+++ work.php
@@ -22,19 +22,8 @@ class Work {
                 break;
 
             case 'list':
-                $list = $_POST['list'];
-                $list = explode("\n", $list);
-
-                $list_words = array();
-
-                foreach($list as $word){
-                    if (!$word = trim($word)) {
-                        continue;
-                    }
-                    $api_result = self::getAPIResult($word);
-                    $api_result['word'] = $word;
-                    $list_words[] = $api_result;
-                }
+                $list = explode("\n", $_POST['list']);
+                $list_words = self::getListResult($list);
 
                 $result .= '<table class="table table-striped">
               <thead>
@@ -62,11 +51,41 @@ class Work {
 
                 }
 
-
                 $result .= '
               </tbody>
             </table>';
+                break;
 
+
+            case 'csv-file':
+                if (isset($_FILES['file']) and $_FILES['file']['error'] == 0) {
+
+                    $file_csv = $_FILES['file']['tmp_name'];
+                    $list = array();
+
+                    $handle = fopen($file_csv, "r");
+                    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                        if (isset($data[0])) {
+                            $list[] = $data[0];
+                        }
+                    }
+                    $list_words = self::getListResult($list);
+
+                    $csv_array = array();
+                    foreach($list_words as $word){
+                        if ($word['success']) {
+                            $csv_array[] = array($word['word'], implode(', ',$word['data']));
+                        }
+                    }
+
+                    self::download_send_headers("data_export_" . date("Y-m-d") . ".csv");
+                    echo self::array2csv($csv_array);
+                    die;
+                }
+                else {
+
+                    $result .= 'File upload error =(';
+                }
                 break;
 
             default:
@@ -74,6 +93,51 @@ class Work {
                 break;
         }
 
+        return $result;
+    }
+
+    static function array2csv(array &$array)
+    {
+        if (count($array) == 0) {
+            return null;
+        }
+        ob_start();
+        $df = fopen("php://output", 'w');
+        foreach ($array as $row) {
+            fputcsv($df, $row, ';');
+        }
+        fclose($df);
+        return ob_get_clean();
+    }
+
+    static function download_send_headers($filename) {
+        // disable caching
+        $now = gmdate("D, d M Y H:i:s");
+        header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+        header("Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate");
+        header("Last-Modified: {$now} GMT");
+
+        // force download
+        header("Content-Type: application/force-download");
+        header("Content-Type: application/octet-stream");
+        header("Content-Type: application/download");
+
+        // disposition / encoding on response body
+        header("Content-Disposition: attachment;filename={$filename}");
+        header("Content-Transfer-Encoding: binary");
+    }
+
+    static function getListResult($list_words){
+        $result = array();
+
+        foreach($list_words as $word){
+            if (!$word = trim($word)) {
+                continue;
+            }
+            $api_result = self::getAPIResult($word);
+            $api_result['word'] = $word;
+            $result[] = $api_result;
+        }
         return $result;
     }
 
