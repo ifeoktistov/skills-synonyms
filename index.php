507371bef9868bb03b7eac4a7047c5f65aa4693c
Igor Feoktistov <feoktistov.igor@gmail.com>
2013-11-26 13:28:03 +0200

    create api test

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+include "work.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>skills-synonyms</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body>
+
+<div class="container">
+    <h3>Hello, skills-synonyms!</h3>
+
+    <?php if (isset($_POST['type'])):?>
+    <div class="alert alert-info">
+        <h4>Server result: </h4>
+        <?php echo Work::processPOST(); ?>
+    </div>
+    <?php endif;?>
+
+    <div class="navbar">
+        <div class="navbar-inner">
+            <form class="navbar-form form-inline" method="post">
+                <label class="checkbox inline">Get synonyms:</label>
+                <input type="text" class="span2" name="word">
+                <input type="hidden" name="type" value="api-test">
+                <button type="submit" class="btn">Test API</button>
+            </form>
+        </div>
+    </div>
+
+</div>
+
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
--- work.php
+++ work.php
@@ -1,1 +1,73 @@
 <?php
+class Work {
+    static $_API_URL = 'http://wikisynonyms.ipeirotis.com/api/';
+
+    static function processPOST(){
+        $type = $_POST['type'];
+        $result = '';
+
+        switch($type) {
+            case 'api-test':
+                $word = $_POST['word'];
+                $terms = self::getAPIResult($word);
+
+                if ($terms['success']) {
+                    $result .= 'Terms: ';
+                    $result .= implode(', ',$terms['data']);
+                }
+                else {
+                    $result .= 'Internal error =( <br/>';
+                    $result .= 'Error message: '.$terms['error_message'];
+                }
+                break;
+
+            default:
+                break;
+        }
+
+        return $result;
+    }
+
+    static function getAPIResult($world){
+        $result = array(
+            'success' => false,
+            'error_message' => '',
+            'data' => array(),
+        );
+        $api_result = @json_decode(self::curlUsingGet(self::$_API_URL.urlencode($world)));
+
+
+
+        if ($api_result and property_exists($api_result,'message') and $api_result->message=='success') {
+            $result['success'] = true;
+            foreach($api_result->terms as $term_data){
+                $result['data'][] = $term_data->term;
+            }
+        }
+        else {
+            $result['error_message'] = 'api request is not success';
+            if ($api_result and property_exists($api_result,'message')) {
+                $result['error_message'] .= ' ('.$api_result->message.')';
+            }
+        }
+
+        return $result;
+    }
+
+
+    static function curlUsingGet($url)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Set curl to return the data instead of printing it to the browser.
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,10); # timeout after 10 seconds, you can increase it
+        curl_setopt($ch, CURLOPT_USERAGENT , "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)");
+        curl_setopt($ch, CURLOPT_URL, $url); #set the url and get string together
+
+        $return = curl_exec($ch);
+        curl_close($ch);
+
+        return $return;
+    }
+}
